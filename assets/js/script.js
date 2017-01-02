2904e4bcdcd1df8910844d6914af4253239a557c
Caitlin Daitch <caitlin.daitch@gmail.com>
2017-01-02 11:56:43 -0500

    Terminal green text

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,12 @@
 			$(".description").slideDown();
 			$(".bottom-navigation").removeClass("inactive");
 			$(".bottom-navigation").addClass("dark-background");
+			$("[auto-type]").addClass("terminal-font");
 			expandedInfo = true;
 		} else {
 			$(".description").slideUp();
 			$(".bottom-navigation").removeClass("dark-background");
+			$("[auto-type]").removeClass("terminal-font");
 			setTimeout(function(){
 				$(".bottom-navigation").addClass("inactive");
 			}, 1000);
