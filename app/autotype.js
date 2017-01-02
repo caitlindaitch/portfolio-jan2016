bdc3efdd555655499b37b4ebd3b037b3e1910edf
Caitlin Daitch <caitlin.daitch@gmail.com>
2017-01-01 23:15:18 -0500

    Terminal and content

---
+++ app/autotype.js
@@ -0,0 +1,30 @@
+app.directive("autoType", ["$timeout", function($timeout){
+	return {
+		restrict: "A",
+		scope: false,
+		link: function(scope, elem, attr){
+			var text = attr.typeContent;
+			var blink = attr.blinkSpeed !== undefined ? attr.blinkSpeed : 300;
+			var timer = null;
+
+			function updateText(elem, i, text){
+				if (i < text.length){
+					elem.html(text.substring(0, i) + " <span class='cursor'>|</span>");
+					i++;
+					timer = $timeout(function(){
+						updateText(elem, i, text);
+					}, blink);
+					return;
+				} else if (i === text.length){
+					elem.html(text.substring(0, i) + " <span class='cursor blink'>|</span>");
+					i++;
+					return;
+				}
+			}
+
+			if (text){
+				updateText(elem, 0, text);
+			}
+		}
+	}
+}]);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 (function(){
-	setTimeout(function(){
-		$("#bottomNav").removeClass("initial-scale");
-	}, 2000);
-
 	var expandedInfo = false;
 
 	function expandCollapseInfo(){
 		if (!expandedInfo) {
 			$(".description").slideDown();
+			$(".bottom-navigation").removeClass("inactive");
+			$(".bottom-navigation").addClass("dark-background");
 			expandedInfo = true;
 		} else {
 			$(".description").slideUp();
+			$(".bottom-navigation").removeClass("dark-background");
+			setTimeout(function(){
+				$(".bottom-navigation").addClass("inactive");
+			}, 1000);
 			expandedInfo = false;
 		}
 	}
