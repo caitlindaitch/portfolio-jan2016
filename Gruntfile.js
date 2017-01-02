2516ea56fde41507d2d32fbf734180ff1f7a5a3c
Caitlin Daitch <caitlin.daitch@gmail.com>
2017-01-02 00:15:09 -0500

    Gruntfile, postCSS

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'assets/css/style.css' : 'assets/css/style.scss'
+				}
+			}
+		},
+		postcss: {
+			options: {
+				processors: [
+					require('autoprefixer')({browsers: ['last 2 versions']}),
+				]
+			},
+			dist: {
+				src: 'assets/css/style.css',
+				dest: 'assets/css/master.css'
+			}
+		},
+		watch: {
+			files: 'assets/css/*.scss',
+			tasks: ['sass', 'postcss']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-postcss');
+
+	grunt.registerTask('dev', ['watch']);
+};
